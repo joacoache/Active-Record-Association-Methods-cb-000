025ff70d844e9d55c24c308d17437137965776e6
Joaquín Herrera <herrerab.joaquin@gmail.com>
2018-07-18 00:22:59 +0000

    automatically backed up by learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -14,6 +14,10 @@ class Genre < ActiveRecord::Base
 
   def all_artist_names
     # return an array of strings containing every musician's name
-    self.artists.name
+    array = []
+    self.artists.each do |artist|
+      array << artist.name
+    end
+    array
   end
 end
